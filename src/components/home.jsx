cb92f90649af30b87ccb7068dfd901defdb912e8
Austine Okongo <austineokongo60@gmail.com>
2022-06-17 20:47:35 +0300

    added home change

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -11,7 +11,7 @@ function Home(){
     <h2>Elevate Your Food <br />Expirience</h2>
     <div  id='homeBtn'>
     <button className='btn'>Get Quote</button>
-    <button className='btn btn-warning'><Link to="/menu">View Menu</Link></button>
+    <Link to="/menu"><button className='btn btn-warning'>View Menu</button></Link>
     </div>
   </div>
   <Smallf/>
